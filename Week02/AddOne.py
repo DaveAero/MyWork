e7c37663d0428f4cdd643f04beea654d2e5e775d
David Burke <davidgerburke@gmail.com>
2022-01-31 16:53:19 +0000

    Week 02, complete promble sheet 2.2

--- Week02/AddOne.py
+++ Week02/AddOne.py
@@ -1,15 +1,14 @@
 # AddOne.py
-# This program prints out the string "Hello"
+# This program reads a number and adds 1
 # author: David Burke
 
-# the users is prompted to enter a number using print
-# the result is saved to a variable called num_in
-print("Enter your number here:")
-num_in = input()
+# the users is prompted to enter a number
+# the result is saved to a variable called num_in as a intiger
+num_in = int(input("Enter your number here:"))
 
 # 1 is added to the variable num_in
 # the result is saved to variable num_out
 num_out = num_in + 1
 
 # the resulting number is printed to the command line
-num_out
+print("Next up is {}".format(num_out))
